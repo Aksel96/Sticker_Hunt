69ea52279b59d326216c83544b8d19fa20df8216
Aksel96 <jonhaksel@gmail.com>
2023-11-04 18:16:36 -0600

    se agrego la logica de elimnar archivos, los ajustes, temas y funciones de cambio de color

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import time
+import queue
 
 import flet as ft
 
 # Colores usados en los efectos
-WHITECOLOR = str(ft.colors.WHITE)
+HOVER_COLOR_HOME = str(ft.colors.WHITE)
+HOVER_COLOR_FAV = str(ft.colors.RED)
 BLACKCOLOR = str(ft.colors.SURFACE_VARIANT)
-TEMPCOLOR = str(ft.colors.RED)
+TEMPCOLOR = str(ft.colors.WHITE)
+BG_COLOR_APPBAR = str(ft.colors.SURFACE_VARIANT)
+BG_COLOR_IMAGES = str(ft.colors.SURFACE_VARIANT)
+
+# Tiempo para borrar de la papelera
+TIEMPODELETE = 5
 
 imagenes = []  # Arreglo temporal de imagenes
+papeleraQ = queue.Queue()  # papelera
+papeleraList = []
 
 
 def on_hover(e):  # Funcion para cambiar el color del fondo cuando entra el mouse en las imagenes
 
-    e.control.bgcolor = WHITECOLOR if e.data == "true" else BLACKCOLOR
+    e.control.bgcolor = HOVER_COLOR_HOME if e.data == "true" else BLACKCOLOR
+    e.control.update()
+
+
+def on_hover_fav(e):
+    e.control.bgcolor = HOVER_COLOR_FAV if e.data == "true" else BLACKCOLOR
+    e.control.update()
+
+
+def on_hover_papelera(e):
+    e.control.bgcolor = HOVER_COLOR_FAV if e.data == "true" else BLACKCOLOR
     e.control.update()
 
 
@@ -22,7 +41,7 @@ def on_click_image(e):  # Respuesta al evento de clic en las imagenes
     e.control.bgcolor = TEMPCOLOR
     e.control.update()
     time.sleep(0.1)
-    e.control.bgcolor = WHITECOLOR
+    e.control.bgcolor = HOVER_COLOR_HOME
     e.control.update()
 
     # Añadimos al arreglo de favoritos la direccion de la imagen
@@ -31,7 +50,7 @@ def on_click_image(e):  # Respuesta al evento de clic en las imagenes
     # Alert dialog de favorito
     dlg_modal = dlg = ft.AlertDialog(
         title=ft.Text("¡Guardado!"), content=ft.Text("Se agrego correctamente a favoritos"),
-        on_dismiss=lambda e: print("Dialog dismissed!")
+        on_dismiss=lambda e: print("Alerta cerrada")
     )
 
     def open_dlg(j):
@@ -40,28 +59,63 @@ def on_click_image(e):  # Respuesta al evento de clic en las imagenes
         j.page.update()
 
     open_dlg(e)
-    print(imagenes)
 
 
-def add_image(i, images):  # Agrega las imagenes a un contenedor
+def on_click_image_favorites(e):  # Respuesta al evento de clic en las imagenes
 
-    images.controls.append(
-        ft.Container(
-            content=
-            ft.Image(
-                src=f"https://picsum.photos/150/150?{i}",
-                fit=ft.ImageFit.NONE,
-                repeat=ft.ImageRepeat.NO_REPEAT,
-                border_radius=ft.border_radius.all(10),
-            ),
-            margin=5,
-            padding=5,
-            alignment=ft.alignment.center,
-            bgcolor=ft.colors.SURFACE_VARIANT,
-            on_hover=on_hover,
-            on_click=on_click_image,
-        )
+    # Animacion de click
+    e.control.bgcolor = TEMPCOLOR
+    e.control.update()
+    time.sleep(0.1)
+    e.control.bgcolor = HOVER_COLOR_FAV
+    e.control.update()
+
+    # Añadimos al arrar de papelera
+    papeleraList.append(e.control.content.src)
+
+    # Eliminamos el elemento de favoritos
+    imagenes.remove(e.control.content.src)
+
+    # Alert dialog de favorito
+    dlg_modal = dlg = ft.AlertDialog(
+        title=ft.Text("¡Aviso!"), content=ft.Text("Se mando el archivo a la papelera"),
+        on_dismiss=lambda e: print("Alerta cerrada")
     )
+    e.control.update()
+
+    def open_dlg(j):
+        j.page.dialog = dlg_modal
+        dlg.open = True
+        j.page.update()
+
+    open_dlg(e)
+
+
+def on_click_image_papelera(e):  # Respuesta al evento de clic en las imagenes
+
+    # Animacion de click
+    e.control.bgcolor = TEMPCOLOR
+    e.control.update()
+    time.sleep(0.1)
+    e.control.bgcolor = HOVER_COLOR_FAV
+    e.control.update()
+
+    # Eliminamos el elemento de la papelera
+    papeleraList.remove(e.control.content.src)
+
+    # Alert dialog de favorito
+    dlg_modal = dlg = ft.AlertDialog(
+        title=ft.Text("¡ELIMINADO!"), content=ft.Text("Se elimino el elemento definitivamente"),
+        on_dismiss=lambda e: print("Alerta cerrada")
+    )
+    e.control.update()
+
+    def open_dlg(j):
+        j.page.dialog = dlg_modal
+        dlg.open = True
+        j.page.update()
+
+    open_dlg(e)
 
 
 def rail_widget(page):  # Widget del rail de la izquierda
@@ -103,7 +157,7 @@ def app_bar_widget():  # Widget del app bar (Barra de arriba)
         title=ft.Text("Sticker Hunt"),
         automatically_imply_leading=True,
         center_title=False,
-        bgcolor=ft.colors.SURFACE_VARIANT,
+        bgcolor=BG_COLOR_APPBAR,
         actions=[
             ft.TextField(label="Buscar sticker:", hint_text="Ingresa tu busqueda:", content_padding=10, ),
             ft.FloatingActionButton(
@@ -111,7 +165,7 @@ def app_bar_widget():  # Widget del app bar (Barra de arriba)
                     [ft.Icon(ft.icons.SEARCH), ft.Text("Buscar")],
                     spacing=10,
                 ),
-                bgcolor=ft.colors.SURFACE_VARIANT,
+                bgcolor=BG_COLOR_APPBAR,
                 shape=ft.RoundedRectangleBorder(radius=10),
                 width=100,
                 mini=True,
@@ -134,14 +188,103 @@ def images_widget():  # Creamos un gridView donde se mostraran las imagenes
     return images
 
 
+def add_image(i, images):  # Agrega las imagenes a un contenedor
+
+    images.controls.append(
+        ft.Container(
+            content=
+            ft.Image(
+                src=f"https://picsum.photos/150/150?{i}",
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10),
+            ),
+            margin=5,
+            padding=5,
+            alignment=ft.alignment.center,
+            bgcolor=BG_COLOR_IMAGES,
+            on_hover=on_hover,
+            on_click=on_click_image,
+        )
+    )
+
+
+def add_image_favorites(i, favorites):
+    favorites.controls.append(
+        ft.Container(
+            content=
+            ft.Image(
+                src=f"{imagenes[i]}",
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10),
+            ),
+            margin=5,
+            padding=5,
+            alignment=ft.alignment.center,
+            bgcolor=BG_COLOR_IMAGES,
+            on_hover=on_hover_fav,
+            on_click=on_click_image_favorites,
+        )
+    )
+
+
+def add_image_papelera(i, papelera):
+    papelera.controls.append(
+        ft.Container(
+            content=
+            ft.Image(
+                src=f"{papeleraList[i]}",
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10),
+            ),
+            margin=5,
+            padding=5,
+            alignment=ft.alignment.center,
+            bgcolor=BG_COLOR_IMAGES,
+            on_hover=on_hover_papelera,
+            on_click=on_click_image_papelera,
+        )
+
+    )
+
+
 def main(page: ft.Page):
+    def theme_changed(e):  # Elegir el tema de la aplicacion
+        page.theme_mode = (
+            ft.ThemeMode.DARK
+            if page.theme_mode == ft.ThemeMode.LIGHT
+            else ft.ThemeMode.LIGHT
+        )
+        c.label = (
+            "Tema claro" if page.theme_mode == ft.ThemeMode.LIGHT else "Tema oscuro"
+        )
+        page.update()
+
+    page.theme_mode = ft.ThemeMode.DARK
+    c = ft.Switch(label="Tema oscuro", on_change=theme_changed)
 
     def route_change(route):  # Funcion para administrar el cambio de views
         page.views.clear()
         images = images_widget()
-        for i in range(0, 70):
+        favoritos = images_widget()
+        papelera = images_widget()
+
+        for i in range(0, 50):
             add_image(i, images)
             page.update()
+
+        if imagenes:
+            for i in range(len(imagenes)):
+                add_image_favorites(i, favoritos)
+                page.update()
+
+        if papeleraList:
+            for i in range(len(papeleraList)):
+                add_image_papelera(i, papelera)
+                page.update()
+
         page.views.append(
             ft.View(
                 "/",  # View del inicio
@@ -154,10 +297,7 @@ def main(page: ft.Page):
                             rail_widget(page),
                         ],
                         expand=True,
-                    ),
-                ],
-            )
-        )
+                    ), ], ))
         if page.route == "/favorite":  # View de los favoritos
             page.views.append(
                 ft.View(
@@ -166,16 +306,12 @@ def main(page: ft.Page):
                         app_bar_widget(),
                         ft.Row(
                             [
-                                ft.Column([ft.Text("Imagenes Favoritas")], alignment=ft.MainAxisAlignment.START,
-                                          expand=True),
+                                ft.Column([favoritos], alignment=ft.MainAxisAlignment.START, expand=True),
                                 ft.VerticalDivider(width=1),
                                 rail_widget(page),
                             ],
                             expand=True,
-                        ),
-                    ],
-                )
-            )
+                        ), ], ))
         if page.route == "/papelera":  # View de la papelera
             page.views.append(
                 ft.View(
@@ -184,16 +320,12 @@ def main(page: ft.Page):
                         app_bar_widget(),
                         ft.Row(
                             [
-                                ft.Column([ft.Text("Papelera")], alignment=ft.MainAxisAlignment.START,
-                                          expand=True),
+                                ft.Column([papelera], alignment=ft.MainAxisAlignment.START, expand=True),
                                 ft.VerticalDivider(width=1),
                                 rail_widget(page),
                             ],
                             expand=True,
-                        ),
-                    ],
-                )
-            )
+                        ), ], ))
         if page.route == "/ajustes":  # View de los ajustes
             page.views.append(
                 ft.View(
@@ -202,16 +334,13 @@ def main(page: ft.Page):
                         app_bar_widget(),
                         ft.Row(
                             [
-                                ft.Column([ft.Text("Ajustes")], alignment=ft.MainAxisAlignment.START,
-                                          expand=True),
+                                ft.Column([c, ft.Text("Copyright 2023")],
+                                          alignment=ft.MainAxisAlignment.START, expand=True),
                                 ft.VerticalDivider(width=1),
                                 rail_widget(page),
                             ],
                             expand=True,
-                        ),
-                    ],
-                )
-            )
+                        ), ], ))
         page.update()
 
     def view_pop():
